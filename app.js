dbb1a640874ef654dd75c08fe3ec439bb4471bbf
SirSnow <hakuhal.snow@gmail.com>
2020-05-11 16:58:53 +0800

    测试一下能不能push到server上去

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('I will try try!')
+app.get("/", (req, res) => {
+  res.send("I will try try try!");
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
