971486db13c71928f8860d1fe7ecf71b82b11dd7
SirSnow <hakuhal.snow@gmail.com>
2020-06-17 12:15:57 +0800

    重新布线了babel，nodemon

--- dist/app.js
+++ dist/app.js
@@ -1,30 +1,28 @@
 "use strict";
 
-var _express = require("express");
-
-var _express2 = _interopRequireDefault(_express);
+var _express = _interopRequireDefault(require("express"));
 
 var _bodyParser = require("body-parser");
 
-var _cors = require("cors");
+var _cors = _interopRequireDefault(require("cors"));
 
-var _cors2 = _interopRequireDefault(_cors);
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var app = (0, _express["default"])(); //middleware
 
-var app = (0, _express2.default)();
+app.use((0, _cors["default"])()); //跨域
 
-//middleware
-app.use((0, _cors2.default)()); //跨域
 app.use((0, _bodyParser.json)()); //json
-app.use((0, _bodyParser.urlencoded)({ extended: true })); //url+params的粘连
 
-var port = 3000;
-//定义get request
+app.use((0, _bodyParser.urlencoded)({
+  extended: true
+})); //url+params的粘连
+
+var port = 3000; //定义get request
+
 app.get("/", function (req, res) {
   res.send("hello try try!");
-});
-//添加一个自己设置的header，status的代码
+}); //添加一个自己设置的header，status的代码
 // app.get("/demo", (req, res) => {
 //   res.set("X-full-stack", "4life");
 //   res.status(418);
@@ -32,6 +30,5 @@ app.get("/", function (req, res) {
 // });
 
 app.listen(port, function () {
-  return console.log("Example app listening at http://localhost:" + port);
+  return console.log("Example app listening at http://localhost:".concat(port));
 });
-//# sourceMappingURL=app.js.map
